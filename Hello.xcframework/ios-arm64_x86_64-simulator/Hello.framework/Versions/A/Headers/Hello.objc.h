3a74d0fec789f216173bc1c127fe190c6da037a9
kevinzhow <kevinchou.c@gmail.com>
2021-06-22 14:48:34 +0800

    feature(framework): bundle

--- Hello.xcframework/ios-arm64_x86_64-simulator/Hello.framework/Versions/A/Headers/Hello.objc.h
+++ Hello.xcframework/ios-arm64_x86_64-simulator/Hello.framework/Versions/A/Headers/Hello.objc.h
@@ -11,6 +11,34 @@
 #include "Universe.objc.h"
 
 
+@class HelloAPOD;
+@class HelloNasaClient;
+
+@interface HelloAPOD : NSObject <goSeqRefInterface> {
+}
+@property(strong, readonly) _Nonnull id _ref;
+
+- (nonnull instancetype)initWithRef:(_Nonnull id)ref;
+- (nonnull instancetype)init;
+@property (nonatomic) NSString* _Nonnull date;
+@property (nonatomic) NSString* _Nonnull explanation;
+@property (nonatomic) NSString* _Nonnull hDurl;
+@property (nonatomic) NSString* _Nonnull mediaType;
+@property (nonatomic) NSString* _Nonnull serviceVersion;
+@property (nonatomic) NSString* _Nonnull title;
+@property (nonatomic) NSString* _Nonnull url;
+@end
+
+@interface HelloNasaClient : NSObject <goSeqRefInterface> {
+}
+@property(strong, readonly) _Nonnull id _ref;
+
+- (nonnull instancetype)initWithRef:(_Nonnull id)ref;
+- (nonnull instancetype)init;
+@property (nonatomic) NSString* _Nonnull apiKey;
+- (HelloAPOD* _Nullable)getAPOD:(NSError* _Nullable* _Nullable)error;
+@end
+
 FOUNDATION_EXPORT NSString* _Nonnull HelloGreetings(NSString* _Nullable name);
 
 #endif
